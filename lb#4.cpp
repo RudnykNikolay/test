dc4613816a100c3a5127b29a4e3e798ac5136972
RudnykNikolay <22fmif.m.rudnyk@std.npu.edu.ua>
2022-12-12 09:02:58 +0200

    lb#4.cpp

---
+++ lb#4.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+//Результат ділення чисел х та у, не перевищує 5, при тому що х
+//дорівнює 20
+
+int main() {
+    int x=20;
+    int y=10;
+    if(x/y<5)
+    {
+        cout<<"Umova vukonana"<< endl;
+        //Умова виконана
+    }
+    if(x/y>5)
+    {
+        cout<<"Umova ne vukonana"<< endl;
+        //Умова не виконана
+    }
+    return 0;
+}
